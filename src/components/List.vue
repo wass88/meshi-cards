c357c32dc96ac9dce50d3dc7e236564170fdd48a
wass80 <wasss80@gmail.com>
2019-04-16 11:19:02 +0900

    Mock shop card

---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <Shop :info="shops[0]"></Shop>
+  </v-container>
+</template>
+
+<script>
+import Shop from "./Shop"
+  const shop_hl = {
+        name: 'ハイライト',
+        open_day: '月火水木金土',
+        open_time: '11:00-22:30',
+        menus: [
+          {name: 'カラフルジャンボチキンカツ', price:'750',
+          img:'https://cdn-ak.f.st-hatena.com/images/fotolife/m/mesitsu_la/20170308/20170308220915.jpg'},
+        ],
+        tags: [
+          "定食", "コスパ", "揚げ物"
+        ],
+        flavor: "コスパの良い揚げ物屋"
+      };
+  export default {
+    data: () => ({
+      shops: [shop_hl]
+    }),
+    components: {
+      Shop
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Shop.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card">
+    <div class="header">
+      <div class="cost">
+        <span> {{info.menus[0].price / 10}} </span>
+      </div>
+      <div class="name">
+      {{ info.name }}
+      </div>
+    </div>
+    <div class="image" :style="{backgroundImage: 'url('+info.menus[0].img+')'}"> </div>
+    <div class="content">
+      {{ info.menus[0].name }} {{ info.menus[0].price }}
+    </div>
+    <div class="flavor">
+      {{ info.flavor }}
+    </div>
+    <div class="footer">
+      {{ info.open }}
+    </div>
+  </div>
+</template>
+
+<style>
+.card {
+  position: relative;
+  box-sizing: border-box;
+  width: 63mm;
+  height: 88mm;
+  border: 3mm black solid;
+  border-radius: 3mm;
+  font-family: 'Kosugi Maru', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 4mm;
+}
+.header {
+  font-size: 6mm;
+}
+.cost {
+  border-radius: 50%;
+  width: 12mm;
+  height: 12mm;
+  display: inline;
+  border: 6px solid black;
+  position: absolute;
+  top: -2mm;
+  left: -2mm;
+}
+.cost > span {
+  font-size: 7mm;
+  margin-left: 4px;
+}
+.name {
+  margin-left: 42px;
+  text-align: center;
+}
+.image {
+  height: 30mm;
+  background-size: cover;
+}
+.content {
+  flex-grow: 1;
+}
+</style>
+
+<script>
+export default {
+  props : [
+    "info"
+  ],
+  data() {
+    return({
+
+    });
+  },
+};
+</script>
